81c43394183df45f891ea6178bbe14c46c84beb5
Omar Morales-Saez (RIT Student) <oym3778@rit.edu>
2024-04-18 15:03:07 -0400

    Serial Communication is communicating

--- Handheld_Earth/Assets/Scripts/SerialCommunication.cs
+++ Handheld_Earth/Assets/Scripts/SerialCommunication.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System;
 
 public class SerialCommunication : MonoBehaviour
 {
+
+    SerialPort stream = new SerialPort("COM3", 9600);
+    int rButton = 0;
+    int lButton = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       stream.Open();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        string value = stream.ReadLine();
+        Debug.Log(value);
     }
 }
