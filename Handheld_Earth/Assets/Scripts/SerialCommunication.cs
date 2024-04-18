45fe8259da9d23681e79ac1f988c5fa99f6c0dd8
Omar Morales-Saez (RIT Student) <oym3778@rit.edu>
2024-04-18 15:19:16 -0400

    splitting the value string coming in from arduino, added comments

--- Handheld_Earth/Assets/Scripts/SerialCommunication.cs
+++ Handheld_Earth/Assets/Scripts/SerialCommunication.cs
@@ -6,21 +6,27 @@ using System;
 
 public class SerialCommunication : MonoBehaviour
 {
-
+    // Set the port amd the baud rate as indicated by the arduino
     SerialPort stream = new SerialPort("COM3", 9600);
+
+    // Variables to be used once received data is split into an array
     int rButton = 0;
     int lButton = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Open the serial stream
        stream.Open();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Roll, Pitch, Yaw, Lbutton, Rbutton
         string value = stream.ReadLine();
-        Debug.Log(value);
+        string[] splitArray = value.Split(",");
+
+        Debug.Log(splitArray[3] + " " + splitArray[4]);
     }
 }
