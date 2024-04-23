26c9a62373a74c605256ce5865464ba535853cdd
Omar Morales-Saez <oym3778@g.rit.edu>
2024-04-22 20:08:12 -0400

    Added NavMeshAgent Package and created some models using the package

---
+++ Handheld_Earth/Assets/Scripts/Dinosaur.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dinosaur : MonoBehaviour
+{
+    [SerializeField] SerialCommunication serial;
+
+    [SerializeField] public Vector3 position = Vector3.zero;
+    [SerializeField] public GameObject target;
+    public NavMeshAgent agent;
+
+    // use later to track all dinosaurs and have each predator follow closest prey
+    //List<int> numbers = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //serial.GetRButton();
+        //serial.GetLButton();
+        position = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.name == "Prey")
+        {
+            // Go the opposite direction of the predator
+            agent.SetDestination(target.transform.position * -1);
+        }
+        else
+        {
+            agent.SetDestination(target.transform.position);
+        }
+        
+
+    }
+}
